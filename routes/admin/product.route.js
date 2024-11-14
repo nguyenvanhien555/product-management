44570acc53e2e582cd58e6475231683eb5b18cc7
Nguyen Hien <hienkoi05@gmail.com>
2024-11-14 15:33:41 +0700

    Product-Management

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -23,6 +23,7 @@ router.post(
 router.get("/edit/:id", controller.edit);
 router.patch("/edit/:id",
   upload.single("thumbnail"),
+  uploadCloud.upload,
   validate.createPost,
   controller.editPatch
 );
